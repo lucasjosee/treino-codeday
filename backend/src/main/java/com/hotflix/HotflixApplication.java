eb16b6cbf5f077903d1ebbc17f501d5bcf48a305
Lucas José Souza Rodrigues <lucas.souza742007@gmail.com>
2026-07-22 17:33:24 -0300

    scaffold Spring Boot API

---
+++ backend/src/main/java/com/hotflix/HotflixApplication.java
@@ -0,0 +1,12 @@
+package com.hotflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotflixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotflixApplication.class, args);
+    }
+}
